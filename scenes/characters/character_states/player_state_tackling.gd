0a27d8155a743e07bfccd338f376f406ef76c176
QuinnHu-star <jzhuquan@foxmail.com>
2025-06-10 20:23:58 +0800

    添加Script-IDE 插件

--- scenes/characters/character_states/player_state_tackling.gd
+++ scenes/characters/character_states/player_state_tackling.gd
@@ -2,16 +2,23 @@ class_name PlayerStateTackling
 extends PlayerState
 
 
-const TACKLE_DURATION := 200
+const TACKLE_RECOVER_DURATION := 200
+const GROUND_FRICTION := 200.0
 
-var tackle_start_time := Time.get_ticks_msec()
+var tackle_recover_start_time := Time.get_ticks_msec()
+var is_tackling := true
 
 
 func _enter_tree() -> void:
 	animation_player.play("tackle")
-	tackle_start_time = Time.get_ticks_msec()
 
 
 func _process(delta: float) -> void:
-	if Time.get_ticks_msec() - tackle_start_time > TACKLE_DURATION:
-		state_transition_requested.emit(Player.State.RECOVERING)
+	if is_tackling:
+		player.velocity = player.velocity.move_toward(Vector2.ZERO, GROUND_FRICTION * delta)
+		if player.velocity == Vector2.ZERO:
+			is_tackling = false
+			tackle_recover_start_time = Time.get_ticks_msec()
+	else:
+		if Time.get_ticks_msec() - tackle_recover_start_time > TACKLE_RECOVER_DURATION:
+			state_transition_requested.emit(Player.State.RECOVERING)
